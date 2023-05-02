2c4c01b994c4f46257f040f055d297321b2ba588
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-05-02 17:50:51 +0800

    Updata 新增標題動畫

--- Unity_Project_Training_Jack_Chiu_2DPlatform_Game_20230310/Assets/Script/MeunManager.cs
+++ Unity_Project_Training_Jack_Chiu_2DPlatform_Game_20230310/Assets/Script/MeunManager.cs
@@ -6,7 +6,7 @@ public class MeunManager : MonoBehaviour
     /// <summary>
     /// Â÷¶}¹CÀ¸
     /// </summary>
-    public void Quit()
+    public void QuitGame()
     {
         Application.Quit();
     }
