4086f87eb386726e10a90180de1372b53ae4b46d
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-06-06 01:22:04 +0800

    Update 修改怪物生成系統

--- Assets/Script/SpawnSystem.cs
+++ Assets/Script/SpawnSystem.cs
@@ -6,24 +6,38 @@ public class SpawnSystem : MonoBehaviour
     [SerializeField] float interval = 0f;
     [Header("怪物預製物")]
     [SerializeField] GameObject prefabEnemy = null;
-    [Header("最大生成點範圍")]
-    [SerializeField] float rangeMax;
-    [Header("最小生成點範圍")]
-    [SerializeField] float rangeMin;
+    [Header("生成點範圍X")]
+    [SerializeField] float range_X;
+    [Header("生成點範圍Y")]
+    [SerializeField] float range_Y;
+    [Header("檢查地面位移")]
+    [SerializeField] Vector2 offset;
     [Header("怪物生成範圍")]
-    [SerializeField] Vector3 monsterRange;
+    private Vector3 monsterRange;
 
     private void Start()
     {
+        // 延遲重複呼叫
         InvokeRepeating("SpawnEnemy", 0, interval);
     }
 
+    // 繪製圖示
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        // 繪製矩形
+        Gizmos.DrawWireCube(new Vector2(transform.position.x, transform.position.y) + offset, new Vector3(range_X, range_Y, 0f));
+    }
+
+    /// <summary>
+    /// 生成怪物
+    /// </summary>
     void SpawnEnemy()
     {
-        float rangeX = Random.Range(rangeMin, rangeMax);
-        float rangeY = Random.Range(rangeMin, rangeMax);
-        Vector3 monsterRange = new Vector2(rangeX, rangeY);
-        
+        float rangeX = Random.Range(range_X, range_Y);
+        float rangeY = Random.Range(range_X, range_Y);
+        monsterRange = new Vector3(rangeX, rangeY);
+        // 於生成範圍內生成怪物
         Instantiate(prefabEnemy, monsterRange, Quaternion.identity);
     }
 }
