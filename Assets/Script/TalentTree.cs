d7b92a0f5d592962317214872fc8b0c28e0e5236
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-06-15 16:36:26 +0800

    Update 新增技能樹系統

--- Assets/Script/TalentTree.cs
+++ Assets/Script/TalentTree.cs
@@ -6,4 +6,13 @@ public class TalentTree : MonoBehaviour
 {
     [Header("技能ID")]
     public int[] id;
+
+    /// <summary>
+    /// 施放法術
+    /// </summary>
+    /// <param name="index"></param>
+    public void CastSkill(int index)
+    {
+        Debug.Log("按下按鈕：" + index + "號技能");
+    }
 }
